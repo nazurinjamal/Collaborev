bb79740e8386700b4f47c793cf36a35e9c44d31c
Alya Kamran <alyakamran82@gmail.com>
2023-11-15 16:44:34 +0800

    CollaboRev app

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AdminController extends Controller
+{
+    public function destroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    } //End Method
+
+    public function Profile(){
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.admin_profile_view', compact('adminData'));
+
+    } //End Method
+
+    public function EditProfile() {
+        $id = Auth::user()->id;
+        $editData = User::find($id);
+        return view('admin.admin_profile_edit', compact('editData'));
+    } //End Method
+}
+
